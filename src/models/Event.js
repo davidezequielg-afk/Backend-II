fd827a40846cef7a9abced39ec9f07547f809648
davidezequielg-afk <davidezequielg@icloud.com>
2026-08-28 09:53:17 -0300

    Actualizacion de Modelos

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,10 +1,10 @@
 import { Schema, model } from 'mongoose';
 
 const eventSchema = new Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    date: { type: Date, required: true },
-    location: { type: String, required: true },
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    date: {type: Date, required: true},
+    location: {type: String, required: true},
 });
 
 const Event = model('Event', eventSchema);
--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,11 @@
 import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    role: {type: String, enum: ['user', 'organizer', 'admin'], default: 'user'},
 });
 
 const User = model('User', userSchema);
