891eacd2a0ac3d71a437b3f2927fa19586181fb0
davidezequielg-afk <davidezequielg@icloud.com>
2026-08-28 09:37:13 -0300

    Acualizacion de STATUS

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,3 +1,3 @@
 export const sessionsController = (req, res) => {
-  res.json({ message: 'Estas son las sesiones en uso' });
+  res.status(200).json({ message: 'Estas son las sesiones en uso' });
 };
