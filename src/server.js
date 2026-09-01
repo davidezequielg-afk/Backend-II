2d50a1a10d6d4805d0bc2b52adbee62108a2ba39
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-01 01:09:02 -0300

    Iniciacion del servidor

--- src/server.js
+++ src/server.js
@@ -4,9 +4,14 @@ import { PORT } from './config/config.port.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
-connectionDB();
+const startServer = async () => {
+  await connectionDB();
+
 
 const port = PORT();
 app.listen(port, () => {
       console.log(`Servidor escuchando en el puerto ${port}`);
-    });
+    })
+};
+
+startServer();
