159912f257e8497c0bd14b3f2867e56b7eb66ff0
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-01 21:17:31 -0300

    Generar Token con JWT

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/config.jwt.js";
 
 export const generateToken = (payload) => {
-    
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+    return token;
 }
