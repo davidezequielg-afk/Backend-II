241a8d400becf923ed2e6dfab2decf275f74efdc
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-01 21:51:11 -0300

    Agregar controlador de login

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,4 +1,4 @@
-import { registerUser } from '../services/sessions.service.js';
+import { registerUser, loginUser } from '../services/sessions.service.js';
 
 export const registerController = async (req, res) => {
   try { const createdUser = await registerUser(req.body);
@@ -6,5 +6,14 @@ export const registerController = async (req, res) => {
   }
   catch (error) {
    res.status(error.statusCode || 500).json({ message: error.message || 'Error al registrar el usuario' });
+  }
+}
+export const loginController = async (req, res) => {
+  try {
+  const { token } = await loginUser(req.body);
+  res.status(200).json({ message: 'Inicio de sesión exitoso' });
+  }
+  catch (error) {
+  res.status(error.statusCode || 500).json({ message: error.message || 'Error al iniciar sesión' });
   }
 }
