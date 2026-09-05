97549e129a2bbde3349dd17ae46082145816715f
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-04 23:15:31 -0300

    Add Cookie de autenticacion

--- src/config/config.jwt.js
+++ src/config/config.jwt.js
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+export const { NODE_ENV } = process.env;
--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,4 +1,5 @@
 import { registerUser, loginUser } from '../services/sessions.service.js';
+import { NODE_ENV } from '../config/config.jwt.js';
 
 export const registerController = async (req, res) => {
   try { const createdUser = await registerUser(req.body);
@@ -11,9 +12,16 @@ export const registerController = async (req, res) => {
 export const loginController = async (req, res) => {
   try {
   const { token } = await loginUser(req.body);
+  res.cookie("currentUser", token, {
+    httpOnly: true,
+    maxAge: 3600000,
+    sameSite:"lax",
+    secure: NODE_ENV === "production"
+  });
   res.status(200).json({ message: 'Inicio de sesión exitoso' });
   }
   catch (error) {
   res.status(error.statusCode || 500).json({ message: error.message || 'Error al iniciar sesión' });
   }
 }
+
