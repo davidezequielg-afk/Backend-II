4d818ccdf8284a8c9ccaf01c547ff9630ec74e5e
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-05 00:42:54 -0300

    Agregar ruta de login

--- src/routes/api/sessions.router.js
+++ src/routes/api/sessions.router.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { registerController } from '../../controllers/sessions.controller.js';
+import { loginController } from '../../controllers/sessions.controller.js';
+
 
 const routerSession = Router();
 
 routerSession.post('/register', registerController);
+routerSession.post('/login', loginController);
 
 export default routerSession;
