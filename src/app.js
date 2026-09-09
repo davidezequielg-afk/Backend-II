047a8f423c02b302ff5cfd68226bd24c5479ded2
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-08 21:20:07 -0300

    refactor: inicializar Passport en la app.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ import eventsRoutes from './routes/api/events.router.js';
 import sessionsRoutes from './routes/api/sessions.router.js';
 import HealthStatus from './routes/api/health.router.js';
 import cookieParser from 'cookie-parser';
-
+import passport from 'passport';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(passport.initialize());
 
 
 app.use('/api/health', HealthStatus);
