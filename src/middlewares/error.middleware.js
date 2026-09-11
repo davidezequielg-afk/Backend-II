4a37e8fe4633a5f556bcc5c687e1bee80c96a9a4
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-11 10:42:09 -0300

    fix(errors): corregir mensaje de usuarios no autenticados

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,6 +1,6 @@
 
 export const errorMiddleware = (err, req, res, next) => {
     const statusCode = err.statusCode || err.status || 500;
-    const message = err.statusCode ? err.message : err.statusCode === 401 ? 'Usuario no autenticado' : err.message || 'Error interno del servidor';
+    const message = err.statusCode ? err.message : statusCode === 401 ? 'Usuario no autenticado' : err.message || 'Error interno del servidor';
     res.status(statusCode).json({ message });
 }
