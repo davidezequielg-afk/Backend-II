982aa4922f945f8aa37ecb2578923ec572777e69
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-14 14:49:24 -0300

    refactor (config): egregar el cambio de segundos a ms en la expiracion del JWT

--- src/config/config.jwt.js
+++ src/config/config.jwt.js
@@ -4,4 +4,5 @@ dotenv.config();
 
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+export const JWT_EXPIRES_IN_MS = JWT_EXPIRES_IN * 1000; 
 export const { NODE_ENV } = process.env;
