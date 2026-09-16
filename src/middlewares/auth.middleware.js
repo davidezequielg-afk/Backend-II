99d17aa3d6289f3c5d8505750efcb76268190493
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-16 14:12:37 -0300

    refactor (middleware): agregar autentificador de token

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import { verifyToken } from '../utils/jwt.utils.js';
+
+export const authenticate = (req, res, next) => {
+    const token = req.cookies.currentUser;
+
+    if (!token) {
+        return res.status(401).json({ 
+            status: 'error', 
+            message: 'No autenticado' 
+        });
+    }
+    try {
+        const user = verifyToken(token);
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(401).json({ 
+            status: 'error', 
+            message: 'No autenticado' 
+        });
+    }
+};
