038b8fb93b05b7655372c12c2eb23bf685089920
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-17 14:49:49 -0300

    refactor(middleware): crear middleware de autorizacion

---
+++ src/middlewares/authorize.middleware.js
@@ -0,0 +1,14 @@
+export const authorize = (...allowedRoles) => {
+    return (req, res, next) => {
+        const userRole = req.user.role;
+
+        if (!allowedRoles.includes(userRole)) {
+            return res.status(403).json({
+                status: 'error',
+                message: 'No tenés permisos para realizar esta acción'
+            });
+        }
+
+        next();
+    };
+};
