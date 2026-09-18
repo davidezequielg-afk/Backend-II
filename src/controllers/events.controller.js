f21788da3cd44b6d7ecbd2fbd14b3ce647918a89
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-18 14:34:06 -0300

    refactor(controller):  agregar controlador de creacion de eventos

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -1,3 +1,21 @@
+import Event from "../models/Event.js";
+
+export const createEventController = async (req, res) => {
+  try {
+    const { title, description, date, location } = req.body;
+    const event = await Event.create({
+      title,
+      description,
+      date,
+      location,
+      organizer: req.user.id
+    });
+    res.status(201).json({ "status": "success", "payload": event });
+  } catch (error) {
+   next(error);
+  }
+};
+
 export const eventsController = (req, res) => {
   res.status(200).json({ "status": "success", "payload": [] });
 };
