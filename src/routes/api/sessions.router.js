4d7d8fc074e4d122f0972dd09e927187964fe188
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-18 14:40:26 -0300

    refactor:(routes): agregar las rutas de creacion de eventos y la ruta que autoriza a quienes pueden crear esos eventos

--- src/routes/api/sessions.router.js
+++ src/routes/api/sessions.router.js
@@ -5,7 +5,9 @@ import { loginController } from '../../controllers/sessions.controller.js';
 import { currentUserController } from '../../controllers/sessions.controller.js';
 import { logoutController } from '../../controllers/sessions.controller.js';
 import { authenticate } from '../../middlewares/auth.middleware.js';
-
+import { authorize } from '../../middlewares/authorize.middleware.js';
+import routerEvent from './events.router.js';
+import { eventsController } from '../../controllers/events.controller.js';
 
 const routerSession = Router();
 
@@ -13,5 +15,7 @@ routerSession.post('/register', passport.authenticate('register' , { session: fa
 routerSession.post('/login', passport.authenticate('login', { session: false }), loginController);
 routerSession.post('/logout', logoutController);
 routerSession.get('/current', authenticate, currentUserController);
+routerEvent.get('/', eventsController);
+routerEvent.post('/', authenticate, authorize('admin', 'organizer'), eventsController);
 
 export default routerSession;
