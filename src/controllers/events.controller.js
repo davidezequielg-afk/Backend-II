59d907f9109d248206b34c3f2a174bcb89aeff90
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-18 23:17:05 -0300

    refactor(controller): agregar autenticador para modificacion de algun evento

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -19,3 +19,23 @@ export const createEventController = async (req, res, next) => {
 export const eventsController = (req, res) => {
   res.status(200).json({ "status": "success", "payload": [] });
 };
+
+export const updateEventController = async (req, res,next) => {
+  try {
+    const findEventUpdate = await Event.findById(req.params.id);
+    if (!findEventUpdate) {
+      return res.status(404).json({ "status": "error", "message": "No se encuentra el evento" });
+    }
+    const userRole = req.user.role;
+    if (
+      req.user.id !== findEventUpdate.organizer.toString() &&
+      userRole !== 'admin'
+    ) {
+      return res.status(403).json({ "status": "error", "message": "No tienes permiso para actualizar este evento" });
+    }
+    const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json({ "status": "success", "payload": updatedEvent });
+  } catch (error) {
+    next(error);
+  }
+};
