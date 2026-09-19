c0447283de2d983f6ffc689e2923e2c2e0a2776f
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-18 23:18:43 -0300

    refactor(router): agregar el endpoint de update de eventos a las rutas

--- src/routes/api/events.router.js
+++ src/routes/api/events.router.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { eventsController , createEventController } from '../../controllers/events.controller.js';
+import { eventsController , createEventController , updateEventController} from '../../controllers/events.controller.js';
 import { authorize } from '../../middlewares/authorize.middleware.js';
 import { authenticate } from '../../middlewares/auth.middleware.js';
 
@@ -8,5 +8,7 @@ const routerEvent = Router();
 
 routerEvent.get('/', eventsController);
 routerEvent.post('/', authenticate, authorize('admin', 'organizer'), createEventController);
+routerEvent.put('/:id', authenticate, authorize('admin', 'organizer'), updateEventController);
+
 
 export default routerEvent;
