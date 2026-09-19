5ebdb69b31748211d960d4865f34844c944d1664
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-18 21:40:13 -0300

    refactor(app): importar rutas de usuario para su uso

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import passport from 'passport';
 
+import HealthStatus from './routes/api/health.router.js';
 import eventsRoutes from './routes/api/events.router.js';
 import sessionsRoutes from './routes/api/sessions.router.js';
-import HealthStatus from './routes/api/health.router.js';
+import routerUser from './routes/api/users.router.js';
 import cookieParser from 'cookie-parser';
 
 
@@ -20,6 +21,7 @@ app.use(passport.initialize());
 app.use('/api/health', HealthStatus);
 app.use('/api/events', eventsRoutes);
 app.use('/api/sessions', sessionsRoutes);
+app.use('/api/users', routerUser);
 
 app.use(errorMiddleware);
 
